3663d853519931dd9a57a4150cc5bbf0a451584e
Bonifase Orwa <orwabonifase@gmail.com>
2018-07-11 17:04:28 +0300

    Fix test configuration

--- jest.config.js
+++ jest.config.js
@@ -3,10 +3,6 @@ module.exports = {
   setupFiles: ["<rootDir>/src/tests/setup.js"],
   moduleFileExtensions: [...defaults.moduleFileExtensions, "ts", "tsx"],
   verbose: true,
-  transform: {
-    "^.+\\.tsx?$": "ts-jest",
-    "^.+\\.(js|jsx)?$": "babel-jest"
-  },
   moduleNameMapper: {
     "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$":
       "<rootDir>/node_modules/jest-css-modules",
